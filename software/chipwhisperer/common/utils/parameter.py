e8f4f2ed4ae9fac50f09c2d3b91b1762f129b14a
Alex DeTrano <alexdetrano@gmail.com>
2018-04-29 13:40:54 -0400

    use new print api with python2

--- software/chipwhisperer/common/utils/parameter.py
+++ software/chipwhisperer/common/utils/parameter.py
@@ -22,6 +22,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
 # =================================================
+from __future__ import print_function
 import logging
 import sys
 import copy
