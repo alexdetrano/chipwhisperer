93d3adae847b0e54cb5432a995e94b90a310aee6
Alex DeTrano <alexdetrano@gmail.com>
2018-05-20 19:03:42 -0400

    choose qrc_resources based on python version

--- software/chipwhisperer/common/ui/CWMainGUI.py
+++ software/chipwhisperer/common/ui/CWMainGUI.py
@@ -22,7 +22,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with chipwhisperer.  If not, see <http://www.gnu.org/licenses/>.
 import logging
-from Qt.QtWidgets import *
 import sys
 import os
 import traceback
@@ -33,6 +32,7 @@ from chipwhisperer.common.ui.logger_widget import LoggingWidget
 try:
     from Qt.QtCore import *
     from Qt.QtGui import *
+    from Qt.QtWidgets import *
 except ImportError as e:
     print("**********************************************")
     print("ERROR: PySide is required for this program.\nTry installing with 'pip install pyside' first.")
@@ -60,7 +60,14 @@ from chipwhisperer.common.ui.TraceManagerDialog import TraceManagerDialog
 from chipwhisperer.common.ui.ProjectTextEditor import ProjectTextEditor
 from chipwhisperer.common.utils import pluginmanager, util
 from chipwhisperer.common.results.base import ResultsBase
-import chipwhisperer.common.ui.qrc_resources
+
+if sys.version_info[0] < 3:
+    # qrc_resources was recompiled for qt5 and python3
+    # basically in python3, we use byte arrays, but in python2, we use strings
+    import chipwhisperer.common.ui.qrc_resources
+else:
+    import chipwhisperer.common.ui.qrc_resources_pysides2_python3
+
 from pyqtgraph.parametertree import ParameterTree
 from chipwhisperer.common.utils.parameter import Parameter
 from chipwhisperer.common.utils import qt_tweaks
